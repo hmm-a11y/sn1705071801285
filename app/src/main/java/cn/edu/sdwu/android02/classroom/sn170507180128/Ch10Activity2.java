8b49494fdbc8c3965e795e62865334b98bc7e1a7
hmm-a11y <2951696462@qq.com>
2020-04-23 15:40:53 +0800

    4/23

--- app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180128/Ch10Activity2.java
+++ app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180128/Ch10Activity2.java
@@ -1,6 +1,7 @@
 package cn.edu.sdwu.android02.classroom.sn170507180128;
 import android.content.Intent;
 import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -36,11 +37,24 @@ public class Ch10Activity2 extends AppCompatActivity {
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode==101){
-            if(resultCode==RESULT_OK){
-                String name= data.getStringExtra("name");
-                Toast.makeText(this,name,Toast.LENGTH_LONG).show();
-            }else{
+        //3.在父Activity中获取返回值
+        //requestCode用来区分哪一个子activity返回的结果
+        if (requestCode == 101) {
+            if (resultCode == RESULT_OK) {
+                //用户点击确认进一步获取数据
+                String name = data.getStringExtra("name");
+                Toast.makeText(this, name, Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "cancel", Toast.LENGTH_SHORT).show();
+            }
+        } else if (requestCode ==102) {
+            //从联系人列表返回的结果
+
+            if (resultCode == RESULT_OK) {
+                //得到联系人的信息（联系人的编号，lookup,uri)
+                String content = data.getDataString();
+                Toast.makeText(this, content, Toast.LENGTH_SHORT).show();
+            } else {
                 Toast.makeText(this, "cancel", Toast.LENGTH_SHORT).show();
             }
         }
@@ -48,6 +62,38 @@ public class Ch10Activity2 extends AppCompatActivity {
     public void web(View view){
         //使用隐式启动方式打开网页
         Intent intent=new Intent(Intent.ACTION_VIEW, Uri.parse("http://baidu.com"));
+        startActivity(intent);
+    }
+    public void contactsList(View view){
+        //查看联系人的列表
+        Intent intent=new Intent(Intent.ACTION_VIEW,Uri.parse("content://contacts/people/"));
+        startActivity(intent);
+    }
+    public void contactsDetail(View view){
+        //查看联系人地明细
+        Intent intent=new Intent(Intent.ACTION_EDIT);
+        intent.setData(Uri.parse("content://contacts/people/1"));
+        startActivity(intent);
+
+    }
+    public void showMap(View view){
+        //打开地图
+        Intent intent=new Intent(Intent.ACTION_VIEW,Uri.parse("geo:50.123,7.1434"));
+        startActivity(intent);
+    }
+    public void showPhoto(View view){
+        Intent intent=new Intent(Intent.ACTION_VIEW,Uri.parse("content://media/external/images/media/"));
+        startActivity(intent);
+    }
+    public void pickContact(View view){
+        //以子activity的形式，打开联系人列表，让用户选择一个联系人后，返回结果
+        Intent intent=new Intent(Intent.ACTION_PICK);//隐式启动
+        intent.setData(ContactsContract.Contacts.CONTENT_URI);
+        startActivityForResult(intent,102);
+    }
+    public void implictStart(View view){
+        Intent intent=new Intent("com.inspur.action2");
+        intent.setData(Uri.parse("abc://inspur.com"));
         startActivity(intent);
     }
 }
---
+++ app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180128/MyService.java
@@ -0,0 +1,50 @@
+package cn.edu.sdwu.android02.classroom.sn170507180128;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MyService extends Service {
+    private MediaPlayer mediaPlayer;
+    public MyService() {
+    }
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        //初始化播放器
+        mediaPlayer=MediaPlayer.create(this,R.raw.wav);
+        mediaPlayer.setLooping(true);
+        try {
+            mediaPlayer.prepare();
+        }catch (Exception e){
+
+        }
+        Log.i(MyService.class.toString(),"onCreate");
+    }
+    @Override
+    public int onStartCommand(Intent intent,int flags,int startId){
+        //主要业务写在本方法中
+        mediaPlayer.start();
+        Log.i(MyService.class.toString(),"onStartCommand");
+        return super.onStartCommand(intent,flags,startId);
+
+    }
+
+    @Override
+    public void onDestroy() {
+        mediaPlayer.stop();
+        mediaPlayer.release();
+        Log.i(MyService.class.toString(),"onDestroy");
+        super.onDestroy();
+
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+}
